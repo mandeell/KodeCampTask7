98a69a9d97ea71aa264303654103680f2ae5a39c
Okiki Nelson <nelsonokiki@gmail.com>
2025-08-28 13:55:10 +0100

    create the student model

---
+++ Student_Management_System/models/student.py
@@ -0,0 +1,12 @@
+from pydantic import EmailStr
+from sqlmodel import SQLModel, Field
+from typing import Optional, List
+
+
+class Student(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    age: int
+    email: EmailStr = Field(unique=True)
+    grades: List[float] = Field(default_factory=list)
+
