a7d276a8c4f333663a898fd4fdf7d78d53d5506a
Okiki Nelson <nelsonokiki@gmail.com>
2025-08-29 10:30:10 +0100

    create the main app file that brings the project together

---
+++ task 2/E-commerce/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from .utils import create_db_and_tables, configure_cors
+from .routers import cart_router, users_router, admin_product_router, public_product_router
+
+app = FastAPI()
+
+configure_cors(app)
+
+app.include_router(users_router)
+app.include_router(cart_router)
+app.include_router(admin_product_router)
+app.include_router(public_product_router)
+
+
+@app.on_event("startup")
+async def startup_event():
+    """Create database tables on startup"""
+    create_db_and_tables()
+
